6362ce54b1b2d1d5e030901aa06d5a8201496742
B-Whitt <34513926+B-Whitt@users.noreply.github.com>
2022-08-27 13:22:47 -0400

    update countUnique

--- Section 5: Problem Solving Patterns/Multiple Pointers Pattern/unique_values/countUniqueValues_bw.js
+++ Section 5: Problem Solving Patterns/Multiple Pointers Pattern/unique_values/countUniqueValues_bw.js
@@ -5,18 +5,37 @@ values in the array. There can be negative numbers in the array they it will alw
 BW Notes:
 - In solving this problem what questions would I ask?
 1 - by unique do you mean numbers that only exist 1 time or only count a number 1 time?
-
+2 - what should the output be? T/F, array of unique values?
 
 */
 
-
+// This is the solution that I came up with NOT using the recommended 'Multiple Pointer' pattern
 function countUniqueValues(arr) {
     // crate empty object
-    // iterate over array - if value is not in
+    let obj = {};
+    // iterate over array - if value is not in array add to object
+    for (let num of arr){
+        //console.log("this is num: " + num);
+        if (!obj.hasOwnProperty(num)){
+            obj[num] = 1;
+        };
+        // console.log("These are your Keys: " + obj.keys); -- Did not work
+        // console.log("This is obj: " + obj) -- Did not work
+        // console.log("These are your Keys: " + Object.keys(obj));
+    };
+    // print unique values/ object keys
+    return console.log("These are your Keys: " + Object.keys(obj));
 }
 
-console.log(countUniqueValues([1, 1, 1, 1, 2, 2]));
 
-/* Time Complexity - 
+
+
+
+
+
+countUniqueValues([1, 11, 1, 17, 2, 2, 5, 5, 3, 8]);
+// console.log(countUniqueValues([1, 1, 1, 1, 2, 2]));
+
+/* Time Complexity - O(n)
 *  Space Complexity - 
 * */
