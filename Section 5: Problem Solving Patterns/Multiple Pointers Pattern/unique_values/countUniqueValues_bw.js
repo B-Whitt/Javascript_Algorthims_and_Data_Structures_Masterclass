799b0fa9e41e449058890744398f98735065852b
B-Whitt <34513926+B-Whitt@users.noreply.github.com>
2022-08-27 10:50:20 -0400

    update uniqueValues

--- Section 5: Problem Solving Patterns/Multiple Pointers Pattern/unique_values/countUniqueValues_bw.js
+++ Section 5: Problem Solving Patterns/Multiple Pointers Pattern/unique_values/countUniqueValues_bw.js
@@ -1,26 +1,22 @@
 /* Count Unique Values
-Implement a function called countUniquevalues  which accepts a sorted array and counts the
-values in the array, There can be negative numbers in the array they it will always be sorted.
+Implement a function called countUniquevalues  which accepts a sorted array and counts the unique
+values in the array. There can be negative numbers in the array they it will always be sorted.
+
+BW Notes:
+- In solving this problem what questions would I ask?
+1 - by unique do you mean numbers that only exist 1 time or only count a number 1 time?
+
+
 */
 
 
 function countUniqueValues(arr) {
-    if (arr.length === 0) {
-        return 0;
-    }
-    let i = 0;
-    for (let j = 1; j < arr.length; j++) {
-
-        if (arr[i] !== arr[j]) {
-            i++;
-            arr[i] = arr[j];
-        }
-    }
-    return i + 1;
+    // crate empty object
+    // iterate over array - if value is not in
 }
 
 console.log(countUniqueValues([1, 1, 1, 1, 2, 2]));
 
-/* Time Complexity -  0(N)
-*  Space Complexity - 0(1)
+/* Time Complexity - 
+*  Space Complexity - 
 * */
