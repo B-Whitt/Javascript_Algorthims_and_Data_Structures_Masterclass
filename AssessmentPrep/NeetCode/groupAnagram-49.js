9a4b45bce1e7bd61587140a2c7c0e1be2a8c1ebd
B-Whitt <34513926+B-Whitt@users.noreply.github.com>
2022-09-27 07:25:10 -0400

    progress grpAnagram

--- AssessmentPrep/NeetCode/groupAnagram-49.js
+++ AssessmentPrep/NeetCode/groupAnagram-49.js
@@ -24,5 +24,29 @@ Constraints:
 */
 
 var groupAnagrams = function(strs) {
-    
+    // loop
+    // populate obj with str1
+    let obj1 = {};
+    // let obj2 = {};
+    let grpArr = [];
+    let j = "test";
+    for (let i in strs){
+        j = strs[i + 1];
+        console.log(strs[i + 1]);
+        for (let char of strs[i]){
+            obj1[char] = obj1[char] + 1 || 1;
+            //console.log(obj1);
+        }
+        
+        // if (obj1.hasOwnProperty()){
+
+        // }
+    }
+    // compare to each, is annagram move to new array
+
+
+    // Input: strs = ["eat","tea","tan","ate","nat","bat"]
+    // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 };
+
+console.log(groupAnagrams(["cat", "dog", "god", "tac", "mouse"]));
