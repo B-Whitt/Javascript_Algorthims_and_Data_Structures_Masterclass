e9d899aa8b9e16ebdb798e3cd4e0ebd5a6dc3956
B-Whitt <34513926+B-Whitt@users.noreply.github.com>
2022-10-16 14:56:33 -0400

    topK update

--- AssessmentPrep/NeetCode/topKFrequent-347.js
+++ AssessmentPrep/NeetCode/topKFrequent-347.js
@@ -1,5 +1,5 @@
 /* 
-47. Top K Frequent Elements
+347. Top K Frequent Elements
 Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
 Example 1:
@@ -28,7 +28,37 @@ Follow up: Your algorithm's time complexity must be better than O(n log n), wher
  * @return {number[]}
  */
  var topKFrequent = function(nums, k) {
+    // how many times does each item appear in array
+    // use counter obj to get the top k
+
+    let objCount = {};
+    let objTopK = {};
+    for(let j=1; j<=k; j++){
+        objTopK[j] = 0;
+        console.log(objTopK, 'objTopK');
+    }
+
+    for(let n of nums){
+        objCount[n] = objCount[n] + 1 || 1;
+        console.log('this is n: ', n);
+    }
+    
+    let keysArr = Object.keys(objCount);
+    for(let iKey of keysArr){
+        // figure out the key with the top k (2) values
+        if(objCount[iKey] > objTopK[1]){
+            objTopK[1] = iKey;
+        } 
+    }
+
+    console.log(objTopK, 'objTopK');
+    
     
 
     
+    console.log(objCount, "objCount");
+    console.log(keysArr, "keysArr");
+
 };
+
+console.log(topKFrequent([1,1,1,2,2,2,2,4,7,7,7,9], 2));
